c69e8baa0f6b97e3078b018473a88851ffc647fa
Katsuhisa Yuasa <berupon@gmail.com>
2017-04-18 23:59:51 +0900

    add test for hsum8x256_ps add CMakeLists.txt

--- avx_hsum.h
+++ avx_hsum.h
@@ -1,5 +1,7 @@
 
-#include <intrin.h>
+
+//#include <intrin.h>
+#include <x86intrin.h>
 
 // in  : ( x3, x2, x1, x0 )
 // out : (  -,  -,  -, x3+x2+x1+x0 )
@@ -170,6 +172,44 @@ inline __m128 hsum4x256_ps(__m256 a, __m256 b, __m256 c, __m256 d) {
     __m128 ret = _mm_blend_ps(_mm256_castps256_ps128(w), upper, 0x0A /* 0b1010 */);
 
     return ret;
+}
+
+inline __m256 hsum8x256_ps(const __m256 &a,
+                           const __m256 &b,
+                           const __m256 &c,
+                           const __m256 &d,
+                           const __m256 &e,
+                           const __m256 &f,
+                           const __m256 &g,
+                           const __m256 &h) {
+    __m256 t0, t1, t2, t3, t4, t5, t6, t7;
+    __m256 tt0, tt1, tt2, tt3;
+    t0 = _mm256_unpacklo_ps(a, b);    // b5,a5,b4,a4, b1,a1,b0,a0
+    t1 = _mm256_unpackhi_ps(a, b);    // b7,a7,b6,a6, b3,a3,b2,a2
+    t2 = _mm256_unpacklo_ps(c, d);    // d5,c5,d4,c4, d1,c1,d0,c0
+    t3 = _mm256_unpackhi_ps(c, d);    // d7,c7,d6,c6, d3,c3,d2,c2
+    t4 = _mm256_unpacklo_ps(e, f);    // f5,e5,f4,e4, f1,e1,f0,e0
+    t5 = _mm256_unpackhi_ps(e, f);    // f7,e7,f6,e6, f3,e3,f2,e2
+    t6 = _mm256_unpacklo_ps(g, h);    // h5,g5,h4,g4, h1,g1,h0,g0
+    t7 = _mm256_unpackhi_ps(g, h);    // h7,g7,h6,g6, h3,g3,h2,g2
+
+    tt0 = _mm256_add_ps(t0, t1);      // b57,a57,b46,a46, b13,a13,b02,a02
+    tt1 = _mm256_add_ps(t2, t3);      // d57,c57,d46,c46, d13,c13,d02,c02
+    tt2 = _mm256_add_ps(t4, t5);      // f57,e57,f46,e46, f13,e13,f02,e02
+    tt3 = _mm256_add_ps(t6, t7);      // h57,g57,h46,g46, h13,g13,h02,g02
+
+    t0 = _mm256_shuffle_ps(tt0, tt1,_MM_SHUFFLE(1,0,1,0)); // d46,c46,b46,a46, d02,c02,b02,a02
+    t1 = _mm256_shuffle_ps(tt0, tt1,_MM_SHUFFLE(3,2,3,2)); // d57,c57,b57,a57, d13,c13,b13,a13
+    t2 = _mm256_shuffle_ps(tt2, tt3,_MM_SHUFFLE(1,0,1,0)); // h46,g46,f46,e46, h02,g02,f02,e02
+    t3 = _mm256_shuffle_ps(tt2, tt3,_MM_SHUFFLE(3,2,3,2)); // h57,g57,f57,e57, h13,g13,f13,e13
+
+    tt0 = _mm256_add_ps(t0, t1);      // d4567,c4567,b4567,a4567, d0123,c0123,b0123,a0123
+    tt1 = _mm256_add_ps(t2, t3);      // h4567,g4567,f4567,e4567, h0123,g0123,f0123,e0123
+
+    t0 = _mm256_permute2f128_ps(tt0, tt1, 0x20); // h0123,g0123,f0123,e0123, d0123,c0123,b0123,a0123
+    t1 = _mm256_permute2f128_ps(tt0, tt1, 0x31); // h4567,g4567,f4567,e4567, d4567,c4567,b4567,a4567
+
+    return _mm256_add_ps(t0, t1);
 }
 
 // in  : ( x3, x2, x1, x0 )
--- avx_hsum_test.cpp
+++ avx_hsum_test.cpp
@@ -1,12 +1,20 @@
 
 #include <stdio.h>
 #include <limits>
+#include <type_traits>
+
 #include "avx_hsum.h"
+
+template < typename T, size_t N >
+size_t countof( T ( & arr )[ N ] )
+{
+    return std::extent< T[ N ] >::value;
+}
 
 void float_test()
 {
-    float fs[8*4];
-    for (size_t i=0; i<_countof(fs); ++i) {
+    float fs[8*8];
+    for (size_t i=0; i<countof(fs); ++i) {
         fs[i] = (float)(i + 1);
     }
 
@@ -69,34 +77,49 @@ void float_test()
             _mm256_loadu_ps(&fs[030])
         );
         _mm_storeu_ps(results, ret);
-        a = results[0];
-        b = fs[0] + fs[1] + fs[2] + fs[3] + fs[4] + fs[5] + fs[6] + fs[7];
-        diff = a - b;
-        pass = abs(diff) <= std::numeric_limits<float>::epsilon();
-
-        a = results[1];
-        b = fs[010] + fs[011] + fs[012] + fs[013] + fs[014] + fs[015] + fs[016] + fs[017];
-        diff = a - b;
-        pass = pass && (abs(diff) <= std::numeric_limits<float>::epsilon());
-
-        a = results[2];
-        b = fs[020] + fs[021] + fs[022] + fs[023] + fs[024] + fs[025] + fs[026] + fs[027];
-        diff = a - b;
-        pass = pass && (abs(diff) <= std::numeric_limits<float>::epsilon());
-
-        a = results[3];
-        b = fs[030] + fs[031] + fs[032] + fs[033] + fs[034] + fs[035] + fs[036] + fs[037];
-        diff = a - b;
-        pass = pass && (abs(diff) <= std::numeric_limits<float>::epsilon());
-
+        pass = true;
+        for (size_t i=0; i<4; ++i) {
+            a = results[i];
+            int base = 8 * i;
+            b = fs[base+0] + fs[base+1] + fs[base+2] + fs[base+3] + fs[base+4] + fs[base+5] + fs[base+6] + fs[base+7];
+            diff = a - b;
+            pass &= abs(diff) <= std::numeric_limits<float>::epsilon();
+        }
         printf("%s hsum4x256_ps\n", pass ? "PASS" : "FAIL");
     }
+
+    {
+        __m256 ret;
+        ret = hsum8x256_ps(
+            _mm256_loadu_ps(&fs[0]),
+            _mm256_loadu_ps(&fs[010]),
+            _mm256_loadu_ps(&fs[020]),
+            _mm256_loadu_ps(&fs[030]),
+            _mm256_loadu_ps(&fs[040]),
+            _mm256_loadu_ps(&fs[050]),
+            _mm256_loadu_ps(&fs[060]),
+            _mm256_loadu_ps(&fs[070])
+        );
+        _mm256_storeu_ps(results, ret);
+        pass = true;
+        for (size_t i=0; i<8; ++i) {
+            a = results[i];
+            int base = 8 * i;
+            b = fs[base+0] + fs[base+1] + fs[base+2] + fs[base+3] + fs[base+4] + fs[base+5] + fs[base+6] + fs[base+7];
+            diff = a - b;
+            pass &= abs(diff) <= std::numeric_limits<float>::epsilon();
+        }
+        printf("%s hsum8x256_ps\n", pass ? "PASS" : "FAIL");
+    }
+
+
 }
+
 
 void double_test()
 {
     double fs[4*4];
-    for (size_t i=0; i<_countof(fs); ++i) {
+    for (size_t i=0; i<countof(fs); ++i) {
         fs[i] = (double)(i + 1);
     }
 
